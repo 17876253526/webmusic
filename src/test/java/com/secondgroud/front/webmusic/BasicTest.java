832630630c551cd367d8a7eb1985674b0162d5a4
chaiye <3156056300@qq.com>
2018-10-08 18:11:26 +0800

    关注和歌曲前100

--- src/test/java/com/secondgroud/front/webmusic/BasicTest.java
+++ src/test/java/com/secondgroud/front/webmusic/BasicTest.java
@@ -2,11 +2,15 @@ package com.secondgroud.front.webmusic;
 
 import com.secondgroud.front.webmusic.entity.User;
 import com.secondgroud.front.webmusic.mapper.UserMapper;
+import com.secondgroud.front.webmusic.service.FocusRedisService;
+import com.secondgroud.front.webmusic.service.SongRedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,6 +18,9 @@ public class BasicTest {
 
     @Autowired
     UserMapper userMapper;
+
+    @Autowired
+    FocusRedisService redisService;
 
     //测试数据库连接
     @Test
@@ -26,4 +33,17 @@ public class BasicTest {
             System.out.println("数据库连接失败（检查application.xml数据源的url,username,password以及端口）");
         }
     }
+
+    @Test
+    public void testFocus() {
+        redisService.focus(1, 2);
+        redisService.focus(1, 3);
+        redisService.focus(1, 4);
+        redisService.focus(2, 4);
+        redisService.focus(3, 4);
+        Set<Object> objects = redisService.focusMeList(4);
+        Set<Object> objects2 = redisService.myFocusList(1);
+        System.out.println(objects);
+    }
+
 }
